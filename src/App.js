c0e4412edd85f165748439468ea346699321ccde
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-12 02:27:45 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
 
+function App() {
+
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+
+  // console.log(firstName);
+  // console.log(lastName);
+  // function changeFirstNameHandler(event) {
+  //   // console.log("printing first name")
+  //   // console.log(event.target.value);
+  //   setFirstName(event.target.value);
+  // }
+  // function changelastNameHandler(event) {
+  //   // console.log("Printing last name")
+  //   // console.log(event.target.value);
+  //   setLastName(event.target.value);
+  // }
